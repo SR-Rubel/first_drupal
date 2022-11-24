9bb63cdf4f9cfb511cabc9fd48bc83df73ec3d7d
RUBEL UDDIN <rubeluddin@RUBELs-MacBook-Pro.local>
2022-11-24 16:24:35 +0600

    rsvplist schema added

--- modules/custom/rsvplist/src/Form/RSVPForm.php
+++ modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * A form to collect an email address for RSVP details
@@ -9,7 +10,8 @@ namespace Drupal\rsvplist\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
- class RSVPForm extends FormBase {
+class RSVPForm extends FormBase
+{
   /**
    * {@inheritdoc}
    */
@@ -17,17 +19,16 @@ use Drupal\Core\Form\FormStateInterface;
   {
     return 'rsvplist_email_form';
   }
-  
+
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state)
   {
     $node = \Drupal::routeMatch()->getParameter('node');
-    if(!is_null($node)) {
+    if (!is_null($node)) {
       $nid = $node->id();
-    }
-    else{
+    } else {
       $nid = 0;
     }
 
@@ -58,10 +59,11 @@ use Drupal\Core\Form\FormStateInterface;
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state){
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
     $value = $form_state->getValue('email');
-    if(!(\Drupal::service('email.validator')->isValid($value))) {
-      $form_state->setErrorByName('email',$this->t('Your email %mail is not valid',['%mail'=>$value]));
+    if (!(\Drupal::service('email.validator')->isValid($value))) {
+      $form_state->setErrorByName('email', $this->t('Your email %mail is not valid', ['%mail' => $value]));
     }
   }
 
@@ -71,7 +73,6 @@ use Drupal\Core\Form\FormStateInterface;
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
     $submitted_email = $form_state->getValue('email');
-    $this->messenger()->addMessage("Hey this form is working! your just entered ".$submitted_email);
+    $this->messenger()->addMessage("Hey this form is working! your just entered " . $submitted_email);
   }
-  
- }
+}
