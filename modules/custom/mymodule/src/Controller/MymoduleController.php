7a71c8f043016c98202ad23ab494fd08db2aae5f
RUBEL UDDIN <rubeluddin@RUBELs-MacBook-Pro.local>
2023-01-17 16:44:29 +0600

    practice entity and hooks

--- modules/custom/mymodule/src/Controller/MymoduleController.php
+++ modules/custom/mymodule/src/Controller/MymoduleController.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Generate a node 
+ * Generate a node
  */
 
 namespace Drupal\mymodule\Controller;
@@ -26,7 +26,6 @@ class MymoduleController extends ControllerBase
       $node->set('field_name', "test node $i");
       $node->set('title', "test node $i");
       $node->set('nid', $i + 100);
-      $node->status = 1;
       $node->save();
     }
   }
@@ -47,6 +46,21 @@ class MymoduleController extends ControllerBase
     // getting all book entity of distinct type
     $authors = Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type'=>'book']);
   }
+
+  // ====practise section =======
+  public function createEntityPractice(){
+    // creating a configuration entity
+    $type = \Drupal::service('plugin.manager.block');
+    dd($type);
+    $details = array('id'=>'progblock','title'=> 'programmed block','plugin'=>$type,'label'=>'progblock');
+    $configEntity = Drupal\block\Entity\Block::create($details);
+//    $configEntity = Drupal::entityTypeManager()->getStorage('block')->load('progblock');
+
+    $configEntity->save();
+//    dd($configEntity);
+    dd($configEntity);
+    // creating a content entity
+  }
   public function test(){
 
     // $entity = \Drupal::entityTypeManager()->getStorage('node')->load(15);
@@ -54,7 +68,7 @@ class MymoduleController extends ControllerBase
     // dd($entity->access('book'));
 
 
-    // $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+    // $nodeStorage = \Drupal::entityTypeManager()->getStorage('node
     // $ids = $nodeStorage->getQuery()
     // ->condition('status', 1)
     // ->condition('type', 'book') // type = bundle id (machine name)
@@ -64,11 +78,15 @@ class MymoduleController extends ControllerBase
     // ->execute();
     // dd($ids);
 
-    $entity = \Drupal::entityTypeManager()->getStorage('node')->load(17);
-    $langcode = Drupal::languageManager()->getLanguage(Language::TYPE_CONTENT);
-    $context = array('operation' => 'node_tokens');
-    $translation = \Drupal::service($entity,'en',$context);
-    dd($translation);
+//    $entity = \Drupal::entityTypeManager()->getStorage('node')->load(17);
+//    $langcode = Drupal::languageManager()->getLanguage(Language::TYPE_CONTENT);
+//    $context = array('operation' => 'node_tokens');
+//    $translation = \Drupal::service($entity,'en',$context);
+//    dd($translation);
+
+
+//    dd(Drupal::entityTypeManager()->getStorage('node')->load(15)->toUrl()->toString());
+    $this->createEntityPractice();
   }
   public function createNodeType(string $type){
     // getting all entity of distinct type (author)
@@ -79,5 +97,12 @@ class MymoduleController extends ControllerBase
     // getting all book entity of distinct type
     $authors = Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type'=>'book']);
   }
+  public function view(){
+    $content = [];
+    $content['name'] = 'My name is Mr.x';
+    return [
+      '#theme' => 'landing',
+      '#content' => $content,
+    ];
+  }
 }
- 
