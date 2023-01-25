a5da5119937f0aa80b10f351d4a7263da35b31cc
RUBEL UDDIN <rubeluddin@RUBELs-MacBook-Pro.local>
2023-01-25 17:49:54 +0600

    cleaning js added

---
+++ themes/custom/bookshop/src/js/app.js
@@ -0,0 +1,9 @@
+// const removeBtns = document.querySelectorAll('.remove-form-cart');
+
+
+
+// removeBtns.forEach(element => {
+//   element.addEventListener('click',() => {
+//     console.log()
+//   })
+// });
