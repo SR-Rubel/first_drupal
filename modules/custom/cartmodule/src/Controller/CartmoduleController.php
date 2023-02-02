24f5b555d91719186f12d9004637c97d40d4f505
RUBEL UDDIN <rubeluddin@RUBELs-MacBook-Pro.local>
2023-02-02 14:17:49 +0600

    card and order module with entity

--- modules/custom/cartmodule/src/Controller/CartmoduleController.php
+++ modules/custom/cartmodule/src/Controller/CartmoduleController.php
@@ -91,13 +91,48 @@ class CartmoduleController extends ControllerBase
     ];
     return $render['#content']['items'];
   }
-  public function confirmed()
+  public function confirmed(Request $request)
   {
+    $order = Node::create(['type' => 'order']);
+    
+    $query = $this->db->select('cartmodule', 'c');
+    $query->fields('c', ['book_id', 'quantity']);
+    $query->condition('uid', $this->user);
+    $result = $query->execute();
+    $total_cost = 0;
+
+    // creating order details for every product added to the cart
+    foreach ($result as $record) {
+      $node = Node::load($record->book_id);
+      $book_price = $node->field_price->value;
+      $total_cost += $book_price * $record->quantity;
+      
+      $order_details = Node::create(['type' => 'order_details']);
+      $order_details->field_books[] = $record->book_id;
+      $order_details->field_integer = $record->quantity;
+      $order_details->title = 'details';
+      $order_details->save();
+      $order->field_order_details[] = ['target_id'=>$order_details->id()];
+    }
+    $order->title = 'order';
+    $order->field_text = $request->request->get('address');
+    $order->field_price = $total_cost;
+    $order->uid = $this->user;
+    $order_res = $order->save();
+
+    // make empty the user cart
+    if($order_res){
+    $res = $this->conn->delete('cartmodule')
+    ->condition('uid', $this->user)
+    ->execute();
+    }
+    // sending mail to user after order
     $mailManager = \Drupal::service('plugin.manager.mail');
     $module = 'cartmodule';
     $key = 'order'; // Replace with Your key
     $to = \Drupal::currentUser()->getEmail();
-    $params['message'] = "Thanks for placing order. Your order tacking number is x33dfhd";
+    $name = \Drupal::currentUser()->getAccount()->name;
+    $params['message'] = "hello $name ! Thanks for placing order. Your order tacking number is ".$order->id();
     $params['title'] = "Your order has been placed";
     $langcode = \Drupal::currentUser()->getPreferredLangcode();
     $send = true;
